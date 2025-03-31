7e07e841e00ce3ae0cd1fc3fa3ae859432f73191
Moin-A <moinqa6@gmail.com>
2025-03-31 22:18:20 +0530

    Spec: added a test to check if intance variable is set for input strings with new line character

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,14 +1,22 @@
-require 'rspec'
-require_relative '../app/string_calculator.rb'
+# frozen_string_literal: true
 
+require 'rspec'
+require_relative '../app/string_calculator'
 
 RSpec.describe StringCalculator do
-    let(:input_string) { '1,2,3' }
-    let(:string_calculator) { StringCalculator.new input_string }
-    describe '#initialize' do
-       it 'sets instance variable' do
-         expect(string_calculator.instance_variable_get(:@input_string)).to eq('1,2,3')
-         expect(string_calculator.instance_variable_get(:@parsed_input)).to eq([1, 2, 3])
-       end
+  let(:input_string) { '1,2,3' }
+  let(:string_calculator) { StringCalculator.new input_string }
+  describe '#initialize' do
+    it 'sets instance variable' do
+      expect(string_calculator.instance_variable_get(:@input_string)).to eq('1,2,3')
+      expect(string_calculator.instance_variable_get(:@parsed_input)).to eq([1, 2, 3])
     end
+    context 'when input string contains new line' do
+      let(:input_string) { '1\n2,3' }
+      it 'sets instance variable' do
+        expect(string_calculator.instance_variable_get(:@input_string)).to eq('1\n2,3')
+        expect(string_calculator.instance_variable_get(:@parsed_input)).to eq([1, 2, 3])
+      end
+    end
+  end
 end
