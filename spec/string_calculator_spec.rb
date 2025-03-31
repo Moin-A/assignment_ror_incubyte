e7af78e1d717b23220af43e64402a0db91f0a703
Moin-A <moinqa6@gmail.com>
2025-03-31 21:36:37 +0530

    Spec: added a test to check if intance variable is set

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,4 +3,12 @@ require_relative '../app/string_calculator.rb'
 
 
 RSpec.describe StringCalculator do
+    let(:input_string) { '1,2,3' }
+    let(:string_calculator) { StringCalculator.new input_string }
+    describe '#initialize' do
+       it 'sets instance variable' do
+         expect(string_calculator.instance_variable_get(:@input_string)).to eq('1,2,3')
+         expect(string_calculator.instance_variable_get(:@parsed_input)).to eq([1, 2, 3])
+       end
+    end
 end
